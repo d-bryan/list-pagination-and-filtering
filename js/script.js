dbb45360d3ba15556989e9d350d35a1387b6d7a7
d-bryan <dylan.g.bryan@gmail.com>
2019-07-23 15:07:17 -0400

    mid project backup

--- js/script.js
+++ js/script.js
@@ -17,6 +17,13 @@ FSJS project 2 - List Filter and Pagination
    scoped to that function.
 ***/
 
+/**
+ * @const {studentList} 
+ * - Holds an array made of list items for student information
+ */
+const studentList = document.getElementsByClassName('student-item');
+// console.log(studentList);
+const itemsPerPage = 10;
 
 
 
@@ -35,15 +42,66 @@ FSJS project 2 - List Filter and Pagination
        "invoke" the function 
 ***/
 
+/**
+ * - Create the `showPage` function to hide all of the items in the 
+   list except for the ten you want to show.
+ * @param {Array} list - List of students passed into the function to be looped over.
+ * @param {Number} page - Counter for the page number array to display number of items per page
+ */
+function showPage (list, page) {
+   const startIndex = (page * itemsPerPage) - itemsPerPage;
+   const endIndex = page * itemsPerPage;
+   let listDisplay = [];
+   let currentPage;
+   
 
-
+   for (let i = 0; i < list.length; i ++){
+      currentPage = list[i];
+      if (i >= startIndex && i < endIndex) {
+         // console.log(studentDetails[i]);
+         // list[i].appendChild(studentDetails[i]);
+         listDisplay.push(currentPage);
+         
+         // console.log(currentPage);
+      }
+      
+   }
+   // console.log(listDisplay);
+   return listDisplay;
+}
+showPage(studentList,2);
 
 /*** 
    Create the `appendPageLinks function` to generate, append, and add 
    functionality to the pagination buttons.
 ***/
 
+/**
+ * Creates and appends functioning pagination links.
+ * @param {Array} list 
+ */
+function appendPageLinks (list) {
+   const arrLength = ((list.length) - 1);
+   const numPages = ((list.length)/itemsPerPage);
+   let endOfList;
+   let parentElement;
+   let buildToPage = '';
+   const buildFooter = () => {
+      const createDiv = document.createElement('div');
+      const createUl = document.createElement('ul');
+      createDiv.className = 'pagination';
+      createDiv.appendChild(createUl);
+      }
 
+      for (let i = 0; i < arrLength; i ++){
+         endOfList = list[i].lastElementChild;
+         
+      }
+   parentElement = endOfList.parentElement;
+   console.log(endOfList);
+   console.log(parentElement);
+}
+appendPageLinks(studentList);
 
 
 
